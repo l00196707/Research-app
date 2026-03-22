140f0ac21c6e450abe18f3193cc90e2eb35eac81
l00196707 <l00196707@atu.ie>
2026-03-22 14:17:02 +0000

    add retries to service b

--- services/service-b/app.py
+++ services/service-b/app.py
@@ -11,12 +11,15 @@ def health():
 
 @app.route("/process")
 def process():
-    try:
-        response = requests.get(SERVICE_C_URL, timeout=2)
-        data = response.json()
-        return jsonify({"processed_value" : data["value"] * 2})
-    except Exception as e:
-        return jsonify({"error": "Service C unavailable"}), 500
-    
+    retries = 3
+    for attempt in range(retries):
+        try:
+            response = requests.get(SERVICE_C_URL, timeout=1)
+            data = response.json()
+            return jsonify({"processed_value" : data["value"] * 2})
+        except Exception:
+            if attempt == retries - 1:
+                return jsonify({"error": "Service C unavailable after retries"}), 500
+        
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5001)
